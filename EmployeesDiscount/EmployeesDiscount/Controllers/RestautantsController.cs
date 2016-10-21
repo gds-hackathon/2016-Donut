b849f18ce6fe7e7d85e8baf7a11ba22d4ba00242
xiangzhu.yang <xiangzhu.yang@dragonsoftbravo.com>
2016-10-21 21:58:58 +0800

    add Reference.cs

---
+++ EmployeesDiscount/EmployeesDiscount/Controllers/RestautantsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeesDiscount.DonutsWebService;
+
+namespace EmployeesDiscount.Controllers
+{
+    public class RestautantsController : Controller
+    {
+        RestaurantS2GD webservice = new RestaurantS2GD();
+
+        // GET: Restautants
+        [AllowAnonymous]
+        public ActionResult RestautantsList()
+        {
+            var restantantslist = webservice.GetRestaurant(10);
+            ViewData["restantantslist"] = restantantslist;
+            return View();
+        }
+
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+    }
+}
---
+++ EmployeesDiscount/EmployeesDiscount/Models/RestautantsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeesDiscount.Models
+{
+    public class RestautantsViewModel
+    {
+            [Required]
+            [Display(Name = "RestautantName")]
+            [Phone]
+            public string RestautantName { get; set; }
+
+            [Required]
+            [Display(Name = "Discount")]
+            public string Discount { get; set; }
+
+    }
+}

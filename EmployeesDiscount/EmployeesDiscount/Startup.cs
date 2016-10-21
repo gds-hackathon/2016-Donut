7554c9605ecbc0801c2b8d6688c4e6ea7c03a882
xiangzhu.yang <xiangzhu.yang@dragonsoftbravo.com>
2016-10-21 15:18:13 +0800

    new project

---
+++ EmployeesDiscount/EmployeesDiscount/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmployeesDiscount.Startup))]
+namespace EmployeesDiscount
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
